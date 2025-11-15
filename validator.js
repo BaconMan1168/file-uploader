46b60a38d7211beedc51805ade3b306d7e4c2aba
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-11-15 20:13:05 +0800

    Added necessary node modules and express-validator middleware

---
+++ validator.js
@@ -0,0 +1,16 @@
+const { body } = require("express-validator");
+
+const validateRegister = [
+    body("username").trim()
+        .isAlpha(),
+    body("password").trim()
+        .notEmpty(),
+    body("confirm-password").custom((value, { req }) => {
+        return value === req.body.password;
+    })
+]
+
+const validateLogin = [
+    body("username").trim()
+        .isAlpha()
+]
