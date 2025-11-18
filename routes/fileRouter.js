1b739cc3e4a1cae9e48e820ff856ac2976660c20
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-11-18 20:30:01 +0800

    Added file upload route

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -3,5 +3,7 @@ const fileRouter = Router();
 const fileController = require('../controllers/fileController')
 
 fileRouter.get('/upload', fileController.getFileForm); 
-//need to add file uplaoding/post logic 
 fileRouter.get('/:folderId/:fileId', fileController.getFileInfo);
+fileRouter.post('/upload', fileController.uploadFile);
+
+module.exports = fileRouter;
